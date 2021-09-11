537dc964a45471edbeb2558cc22b7484202fd3ac
Herocode <thaiduong61130@gmail.com>
2021-09-11 13:46:53 +0700

    listen

---
+++ main.js
@@ -0,0 +1,39 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const songs = [{
+        name: "Ren Remix uuu",
+        singer: "Raftaar x Fortnite",
+        path: "./mp3/1.mp3",
+        image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg"
+    }, {
+        name: "Tu Phir Se Aana",
+        singer: "Raftaar x Salim Merchant x Karma",
+        path: "./mp3/2.mp3",
+        image: "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg"
+    }, {
+        name: "Naachne Ka Shaunq",
+        singer: "Raftaar x Brobha V",
+        path: "./mp3/3.mp3",
+        image: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg"
+    }, {
+        name: "Mantoiyat",
+        singer: "Raftaar x Nawazuddin Siddiqui",
+        path: "./mp3/4.mp3",
+        image: "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg"
+    }, {
+        name: "Aage Chal",
+        singer: "Raftaar",
+        path: "./mp3/5.mp3",
+        image: "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg"
+    }, {
+        name: "Damn",
+        singer: "Raftaar x kr$na",
+        path: "./mp3/6.mp3",
+        image: "https://filmisongs.xyz/wp-content/uploads/2020/07/Damn-Song-Raftaar-KrNa.jpg"
+    }, {
+        name: "Feeling You",
+        singer: "Raftaar x Harjas",
+        path: "./mp3/7.mp3",
+        image: "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
+    }],
